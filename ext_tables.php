9600321f16f51e8ce408cfd723425a4cd978fd5e
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-31 11:10:20 +0000

    wizicon added
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@46020 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -118,6 +118,8 @@ $TCA['tx_kesearch_indexerconfig'] = array (
 	),
 );
 
-
+if (TYPO3_MODE == 'BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_kesearch_pi1_wizicon.php';
+}
 
 ?>
