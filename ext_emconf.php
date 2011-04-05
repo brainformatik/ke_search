92f6ae2a803059e6effaeff2b3f916d9f67968c4
froemken <froemken@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-05 06:16:31 +0000

    - requires xajax 0.2.5 now
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@46221 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'xajax' => '',
+			'xajax' => '0.2.5-0.2.99',
 		),
 		'conflicts' => array(
 		),
