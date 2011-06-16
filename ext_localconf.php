a7a854b7397463e34f7fe39ee930dc6c02b16b14
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-16 09:42:13 +0000

    - plugin summary call removed from ext_localconf.php (not needed any more since splitted in pi1 and pi2) - updated pi1 name in locallang_db.xml
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@48758 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,9 +9,6 @@ include_once(t3lib_extMgm::extPath($_EXTKEY).'/classes/class.user_filterlist.php
 // include pageTSconfig.txt
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ke_search/pageTSconfig.txt">');
 
-// change code field layout
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['ke_search_pi1']['ke_userregister']='EXT:ke_search/lib/class.tx_kesearch_cms_layout.php:tx_kesearch_cms_layout->getExtensionSummary';
-
 // register cli-script
 if (TYPO3_MODE=='BE')    {
     $TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array('EXT:'.$_EXTKEY.'/cli/class.cli_kesearch.php','_CLI_kesearch');
@@ -22,7 +19,7 @@ t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_kesearch_pi1.php', '_pi1', 'li
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi2/class.tx_kesearch_pi2.php', '_pi2', 'list_type', 0);
 
 // use hooks for generation of sortdate values
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerAdditionalFields'][] = 'EXT:ke_search/hooks/class.user_kesearchhooks.php:user_kesearch_sortdate'; 
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerAdditionalFields'][] = 'EXT:ke_search/hooks/class.user_kesearchhooks.php:user_kesearch_sortdate';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'][] = 'EXT:ke_search/hooks/class.user_kesearchhooks.php:user_kesearch_sortdate';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyNewsIndexEntry'][] = 'EXT:ke_search/hooks/class.user_kesearchhooks.php:user_kesearch_sortdate';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyYACIndexEntry'][] = 'EXT:ke_search/hooks/class.user_kesearchhooks.php:user_kesearch_sortdate';
