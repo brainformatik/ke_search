4772450732a50b7d31d699b594a798461a9544a7
froemken <froemken@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-28 21:09:24 +0000

    many changes in indexer object
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@52544 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Tests/indexer/class.tx_kesearch_indexerTest.php
@@ -0,0 +1,54 @@
+<?php
+class IndexerTest extends Tx_Extbase_BaseTestCase {
+
+	/**
+	 * @var tx_kesearch_indexer
+	 */
+	var $indexer;
+	
+	
+	
+	
+	
+	public function setUp() {
+		$this->indexer = t3lib_div::makeInstance('tx_kesearch_indexer');
+		$this->indexer->additionalFields = array('title', 'orig_uid', 'orig_pid', 'enddate');
+		
+	}
+	
+	public function tearDown() {
+		unset($this->indexer);
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Test additional query parts for additional fields
+	 *
+	 * @test
+	 */
+	public function checkAddQueryPartsForAdditionalFields() {
+		$now = time();
+		$fieldValues = array(
+			'tstamp' => $now,
+			'crdate' => $now,
+			'title' => 'tolle Überschrift',
+			'orig_uid' => 213,
+			'orig_pid' => 423,
+			'enddate' => $now,
+		);
+		$fieldValues = $GLOBALS['TYPO3_DB']->fullQuoteArray($fieldValues, 'tx_kesearch_index');
+		
+		$shouldArray = array(
+			'set' => ', @title = \'tolle Überschrift\', @orig_uid = \'213\', @orig_pid = \'423\', @enddate = \'' . $now . '\'',
+			'execute' => ', @title, @orig_uid, @orig_pid, @enddate'
+		);
+		
+		$isArray = $this->indexer->getQueryPartForAdditionalFields($fieldValues);
+		
+		$this->assertEquals($shouldArray, $isArray);
+	}
+}
+?>
