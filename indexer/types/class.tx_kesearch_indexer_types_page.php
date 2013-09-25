4ac64c5297a1afa3cf6f02d7e37153ddb8a92d66
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-25 08:28:19 +0000

    fal indexer first part
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@80015 735d13b6-9817-0410-8766-e36946ffe9aa

--- indexer/types/class.tx_kesearch_indexer_types_page.php
+++ indexer/types/class.tx_kesearch_indexer_types_page.php
@@ -1,76 +1,86 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Andreas Kiefer (kennziffer.com) <kiefer@kennziffer.com>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
 
-require_once(t3lib_extMgm::extPath('ke_search').'indexer/class.tx_kesearch_indexer_types.php');
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010 Andreas Kiefer (kennziffer.com) <kiefer@kennziffer.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+require_once(t3lib_extMgm::extPath('ke_search') . 'indexer/class.tx_kesearch_indexer_types.php');
 
 /**
  * Plugin 'Faceted search' for the 'ke_search' extension.
  *
  * @author	Andreas Kiefer (kennziffer.com) <kiefer@kennziffer.com>
- * @author	Stefan Froemken (kennziffer.com) <froemken@kennziffer.com>
+ * @author	Stefan Froemken 
+ * @author	Christian Bülter (kennziffer.com) <buelter@kennziffer.com>
  * @package	TYPO3
  * @subpackage	tx_kesearch
  */
 class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
-	var $pids              = 0;
-	var $pageRecords       = array(); // this array contains all data of all pages
+
+	var $pids = 0;
+	var $pageRecords = array(); // this array contains all data of all pages
 	var $cachedPageRecords = array(); // this array contains all data of all pages, but additionally with all available languages
-	var $sysLanguages      = array();
-	var $indexCTypes       = array(
-		'text',
-		'textpic',
-		'bullets',
-		'table',
-		'html',
-		'header'
+	var $sysLanguages = array();
+	var $indexCTypes = array(
+	    'text',
+	    'textpic',
+	    'bullets',
+	    'table',
+	    'html',
+	    'header',
+	    'uploads'
 	);
+	var $fileCTypes = array('uploads');
 	var $counter = 0;
 	var $whereClauseForCType = '';
-
 	// Name of indexed elements. Will be overwritten in content element indexer.
 	var $indexedElementsName = 'pages';
 
-
-
-
+	/**
+	 * @var \TYPO3\CMS\Core\Resource\FileRepository
+	 */
+	var $fileRepository;
 
 	/**
-	 * Construcor of this object
+	 * Constructor of this object
 	 */
 	public function __construct($pObj) {
 		parent::__construct($pObj);
 
 		$this->counter = 0;
-		foreach($this->indexCTypes as $value) {
+		foreach ($this->indexCTypes as $value) {
 			$cTypes[] = 'CType="' . $value . '"';
 		}
 		$this->whereClauseForCType = implode(' OR ', $cTypes);
 
 		// get all available sys_language_uid records
 		$this->sysLanguages = t3lib_BEfunc::getSystemLanguages();
-	}
 
+
+		// make file repository instance
+		// TODO: do it only when the indexer is set to index files, too and
+		// TYPO3 version is >= 6.0
+		$this->fileRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\FileRepository');
+	}
 
 	/**
 	 * This function was called from indexer object and saves content to index table
@@ -94,8 +104,9 @@ class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
 		$this->addTagsToRecords($indexPids, $where);
 
 		// loop through pids and collect page content and tags
-		foreach($indexPids as $uid) {
-			if($uid) $this->getPageContent($uid);
+		foreach ($indexPids as $uid) {
+			if ($uid)
+				$this->getPageContent($uid);
 		}
 
 		// show indexer content?
@@ -108,7 +119,6 @@ class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
 
 		return $content;
 	}
-
 
 	/**
 	 * get array with all pages
@@ -131,13 +141,12 @@ class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
 
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields, $table, $where);
 
-		while($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
 			$this->addLocalizedPagesToCache($row);
 			$pages[$row['uid']] = $row;
 		}
 		return $pages;
 	}
-
 
 	/**
 	 * add localized page records to a cache/globalArray
@@ -148,22 +157,17 @@ class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
 	 */
 	public function addLocalizedPagesToCache($row) {
 		$this->cachedPageRecords[0][$row['uid']] = $row;
-		foreach($this->sysLanguages as $sysLang) {
+		foreach ($this->sysLanguages as $sysLang) {
 			list($pageOverlay) = t3lib_BEfunc::getRecordsByField(
-				'pages_language_overlay',
-				'pid',
-				$row['uid'],
-				'AND sys_language_uid=' . intval($sysLang[1])
+					'pages_language_overlay', 'pid', $row['uid'], 'AND sys_language_uid=' . intval($sysLang[1])
 			);
-			if($pageOverlay) {
+			if ($pageOverlay) {
 				$this->cachedPageRecords[$sysLang[1]][$row['uid']] = t3lib_div::array_merge(
-					$row,
-					$pageOverlay
+						$row, $pageOverlay
 				);
 			}
 		}
 	}
-
 
 	/**
 	 * creates a rootline and searches for valid feGroups
@@ -173,36 +177,37 @@ class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
 	 */
 	public function getRecursiveFeGroups($uid) {
 		$tempRootline[] = $this->cachedPageRecords[0][$uid]['uid'];
-		while($this->cachedPageRecords[0][$uid]['pid']) {
+		while ($this->cachedPageRecords[0][$uid]['pid']) {
 			$uid = $this->cachedPageRecords[0][$uid]['pid'];
-			if(is_array($this->cachedPageRecords[0][$uid])) {
+			if (is_array($this->cachedPageRecords[0][$uid])) {
 				$tempRootline[] = $this->cachedPageRecords[0][$uid]['uid'];
 			}
 		}
 		krsort($tempRootline);
-		foreach($tempRootline as $page) {
+		foreach ($tempRootline as $page) {
 			$rootline[] = $page;
 		}
 		// now we have a full rootline of the current page. 0 = level 0, 1 = level 1 and so on
 		$extendToSubpages = false;
-		foreach($rootline as $uid) {
-			if($this->cachedPageRecords[0][$uid]['extendToSubpages'] || $extendToSubpages) {
-				if(!empty($this->cachedPageRecords[0][$uid]['fe_group'])) {
+		foreach ($rootline as $uid) {
+			if ($this->cachedPageRecords[0][$uid]['extendToSubpages'] || $extendToSubpages) {
+				if (!empty($this->cachedPageRecords[0][$uid]['fe_group'])) {
 					$tempFeGroups = explode(',', $this->cachedPageRecords[0][$uid]['fe_group']);
-					foreach($tempFeGroups as $group) {
+					foreach ($tempFeGroups as $group) {
 						$feGroups[] = $group;
 					}
 				}
 				$extendToSubpages = true;
 			} else {
-				if(!empty($this->cachedPageRecords[0][$uid]['fe_group'])) {
+				if (!empty($this->cachedPageRecords[0][$uid]['fe_group'])) {
 					$feGroups = explode(',', $this->cachedPageRecords[0][$uid]['fe_group']);
-				} else $feGroups = array();
+				}
+				else
+					$feGroups = array();
 			}
 		}
 		return $feGroups;
 	}
-
 
 	/**
 	 * get content of current page and save data to db
@@ -210,96 +215,158 @@ class tx_kesearch_indexer_types_page extends tx_kesearch_indexer_types {
 	 */
 	public function getPageContent($uid) {
 		// get content elements for this page
-		$fields = 'header, bodytext, CType, sys_language_uid, header_layout';
+		$fields = 'uid, header, bodytext, CType, sys_language_uid, header_layout';
 		$table = 'tt_content';
 		$where = 'pid = ' . intval($uid);
-		$where .= ' AND (' . $this->whereClauseForCType. ')';
+		$where .= ' AND (' . $this->whereClauseForCType . ')';
 		$where .= t3lib_BEfunc::BEenableFields($table);
 		$where .= t3lib_BEfunc::deleteClause($table);
 
 		// if indexing of content elements with restrictions is not allowed
 		// get only content elements that have empty group restrictions
-		if($this->indexerConfig['index_content_with_restrictions'] != 'yes') {
+		if ($this->indexerConfig['index_content_with_restrictions'] != 'yes') {
 			$where .= ' AND (fe_group = "" OR fe_group = "0") ';
 		}
 
 		$rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows($fields, $table, $where);
-		if(count($rows)) {
-			foreach($rows as $row) {
-				// header
+		if (count($rows)) {
+			foreach ($rows as $row) {
+				$content = '';
+
+				// index header
 				// add header only if not set to "hidden"
 				if ($row['header_layout'] != 100) {
-					$pageContent[$row['sys_language_uid']] .= strip_tags($row['header']) . "\n";
+					$content .= strip_tags($row['header']) . "\n";
 				}
 
-				// bodytext
-				$bodytext = $row['bodytext'];
-
-				// following lines prevents having words one after the other like: HelloAllTogether
-				$bodytext = str_replace('<td', ' <td', $bodytext);
-				$bodytext = str_replace('<br', ' <br', $bodytext);
-				$bodytext = str_replace('<p', ' <p', $bodytext);
-				$bodytext = str_replace('<li', ' <li', $bodytext);
-
-				if ($row['CType'] == 'table') {
-					// replace table dividers with whitespace
-					$bodytext = str_replace('|', ' ', $bodytext);
+				// index file uploads or normal content
+				if (in_array($row['CType'], $this->fileCTypes)) {
+					$content .= $this->getIndexContentFromAttachedFiles($row) . "\n";
+				} else {
+					$content .= $this->getIndexContentFromContentElement($row) . "\n";
 				}
-				$bodytext = strip_tags($bodytext);
 
-				$pageContent[$row['sys_language_uid']] .= $bodytext."\n";
+				$pageContent[$row['sys_language_uid']] .= $content;
 			}
 			$this->counter++;
 		} else {
 			return;
 		}
 
-			// get Tags for current page
+		// get Tags for current page
 		$tags = $this->pageRecords[intval($uid)]['tags'];
 
-			// make it possible to modify the indexerConfig via hook
+		// make it possible to modify the indexerConfig via hook
 		$indexerConfig = $this->indexerConfig;
 
-			// hook for custom modifications of the indexed data, e. g. the tags
-		if(is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'])) {
-			foreach($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'] as $_classRef) {
+		// hook for custom modifications of the indexed data, e. g. the tags
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'])) {
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'] as $_classRef) {
 				$_procObj = & t3lib_div::getUserObj($_classRef);
 				$_procObj->modifyPagesIndexEntry(
-					$uid,
-					$pageContent,
-					$tags,
-					$this->cachedPageRecords,
-					$additionalFields,
-					$indexerConfig
+					$uid, $pageContent, $tags, $this->cachedPageRecords, $additionalFields, $indexerConfig
 				);
 			}
 		}
 
 		// store record in index table
-		foreach($pageContent as $langKey => $content) {
+		foreach ($pageContent as $langKey => $content) {
 			$this->pObj->storeInIndex(
-				$indexerConfig['storagepid'],                          // storage PID
-				$this->cachedPageRecords[$langKey][$uid]['title'],     // page title
-				'page',                                                // content type
-				$uid,                                                  // target PID: where is the single view?
-				$content,                                              // indexed content, includes the title (linebreak after title)
-				$tags,                                                 // tags
-				'',                                                    // typolink params for singleview
-				$this->cachedPageRecords[$langKey][$uid]['abstract'],  // abstract
-				$langKey,                                              // language uid
+				$indexerConfig['storagepid'], // storage PID
+				$this->cachedPageRecords[$langKey][$uid]['title'], // page title
+				'page', // content type
+				$uid, // target PID: where is the single view?
+				$content, // indexed content, includes the title (linebreak after title)
+				$tags, // tags
+				'', // typolink params for singleview
+				$this->cachedPageRecords[$langKey][$uid]['abstract'], // abstract
+				$langKey, // language uid
 				$this->cachedPageRecords[$langKey][$uid]['starttime'], // starttime
-				$this->cachedPageRecords[$langKey][$uid]['endtime'],   // endtime
-				implode(',', $this->getRecursiveFeGroups($uid)),       // fe_group
-				false,                                                 // debug only?
-				$additionalFields                                      // additional fields added by hooks
+				$this->cachedPageRecords[$langKey][$uid]['endtime'], // endtime
+				implode(',', $this->getRecursiveFeGroups($uid)), // fe_group
+				false, // debug only?
+				$additionalFields				      // additional fields added by hooks
 			);
 		}
 
 		return;
 	}
+
+	/**
+	 *
+	 * Extracts content from files attached to a content element and
+	 * returns it as plain text for writing it directly to the index
+	 *
+	 * @author Christian Bülter <buelter@kennziffer.com>
+	 * @since 24.09.13
+	 * @param array $row content element
+	 * @return string
+	 */
+	public function getIndexContentFromAttachedFiles($row) {
+		$content = '';
+
+		// TODO:
+		// check if we should get the  localized uid of the content element
+		// at this point
+
+		// get files attached to the content element
+		$fileObjects = $this->fileRepository->findByRelation('tt_content', 'media', $row['uid']);
+
+		// loop through files
+		/* @var $fileObject \TYPO3\CMS\Core\Resource\FileReference */
+		foreach ($fileObjects as $fileObject) {
+
+			// check if the file extension fits in the list of extensions
+			// to index defined in the indexer configuration
+			$fileExt = $fileObject->getExtension();
+
+			// fetch the file content
+			$fileUri = $fileObject->getStorage()->getPublicUrl($fileObject);
+
+			// add usergroup restrictions of the page and the
+			// content element to the index data
+
+			// add time restrictions to the index data
+
+			// write file data to the index as a seperate index entry
+
+			// count indexed files, add it to the indexer output
+		}
+
+		return $content;
+	}
+
+	/**
+	 *
+	 * Extracts content from content element and returns it as plain text
+	 * for writing it directly to the index
+	 *
+	 * @author Christian Bülter <buelter@kennziffer.com>
+	 * @since 24.09.13
+	 * @param array $row content element
+	 * @return string
+	 */
+	public function getIndexContentFromContentElement($row) {
+		// bodytext
+		$bodytext = $row['bodytext'];
+
+		// following lines prevents having words one after the other like: HelloAllTogether
+		$bodytext = str_replace('<td', ' <td', $bodytext);
+		$bodytext = str_replace('<br', ' <br', $bodytext);
+		$bodytext = str_replace('<p', ' <p', $bodytext);
+		$bodytext = str_replace('<li', ' <li', $bodytext);
+
+		if ($row['CType'] == 'table') {
+			// replace table dividers with whitespace
+			$bodytext = str_replace('|', ' ', $bodytext);
+		}
+		$bodytext = strip_tags($bodytext);
+		return $bodytext;
+	}
+
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ke_search/indexer/types/class.tx_kesearch_indexer_types_page.php'])	{
+if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ke_search/indexer/types/class.tx_kesearch_indexer_types_page.php']) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ke_search/indexer/types/class.tx_kesearch_indexer_types_page.php']);
 }
 ?>
