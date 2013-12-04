6d589b4c707f3b017b6dce97483a7d000b1e660d
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-04 15:35:10 +0000

    code cleanup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@81900 735d13b6-9817-0410-8766-e36946ffe9aa

--- indexer/types/class.tx_kesearch_indexer_types_ttnews.php
+++ indexer/types/class.tx_kesearch_indexer_types_ttnews.php
@@ -1,28 +1,29 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Andreas Kiefer (kennziffer.com) <kiefer@kennziffer.com>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
 
-require_once(t3lib_extMgm::extPath('ke_search').'indexer/class.tx_kesearch_indexer_types.php');
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010 Andreas Kiefer (kennziffer.com) <kiefer@kennziffer.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+require_once(t3lib_extMgm::extPath('ke_search') . 'indexer/class.tx_kesearch_indexer_types.php');
 
 /**
  * Plugin 'Faceted search' for the 'ke_search' extension.
@@ -41,7 +42,6 @@ class tx_kesearch_indexer_types_ttnews extends tx_kesearch_indexer_types {
 		parent::__construct($pObj);
 	}
 
-
 	/**
 	 * converts the datetime of a record into variables you can use in realurl
 	 *
@@ -50,12 +50,12 @@ class tx_kesearch_indexer_types_ttnews extends tx_kesearch_indexer_types {
 	 */
 	function getParamsForHrDateSingleView($tstamp) {
 
-		if($this->conf['useHRDatesSingle']) {
+		if ($this->conf['useHRDatesSingle']) {
 			$params = array('tx_ttnews' => array(
 				'year' => date('Y', $tstamp),
 				'month' => date('m', $tstamp),
 			));
-			if(!$this->conf['useHRDatesSingleWithoutDay']) {
+			if (!$this->conf['useHRDatesSingleWithoutDay']) {
 				$params['tx_ttnews']['day'] = date('d', $tstamp);
 			}
 		} else {
@@ -63,7 +63,6 @@ class tx_kesearch_indexer_types_ttnews extends tx_kesearch_indexer_types {
 		}
 		return $params;
 	}
-
 
 	/**
 	 * This function was called from indexer object and saves content to index table
@@ -76,55 +75,50 @@ class tx_kesearch_indexer_types_ttnews extends tx_kesearch_indexer_types {
 		$this->conf['useHRDatesSingle'] = $this->indexerConfig['index_news_useHRDatesSingle'];
 		$this->conf['useHRDatesSingleWithoutDay'] = $this->indexerConfig['index_news_useHRDatesSingleWithoutDay'];
 
-			// get all the tt_news entries to index
-			// don't index hidden or deleted news, BUT
-			// get the news with frontend user group access restrictions
-			// or time (start / stop) restrictions.
-			// Copy those restrictions to the index.
+		// get all the tt_news entries to index
+		// don't index hidden or deleted news, BUT
+		// get the news with frontend user group access restrictions
+		// or time (start / stop) restrictions.
+		// Copy those restrictions to the index.
 		$fields = '*';
 		$table = 'tt_news';
 		$indexPids = $this->getPidList($this->indexerConfig['startingpoints_recursive'], $this->indexerConfig['sysfolder'], $table);
-		if($this->indexerConfig['index_use_page_tags']) {
+		if ($this->indexerConfig['index_use_page_tags']) {
 			// add the tags of each page to the global page array
 			$this->pageRecords = $this->getPageRecords($indexPids);
 			$this->addTagsToRecords($indexPids);
 		}
 
 		$where = 'pid IN (' . implode(',', $indexPids) . ') ';
-		$where .= t3lib_befunc::BEenableFields($table,$inv=0);
-		$where .= t3lib_befunc::deleteClause($table,$inv=0);
-		$groupBy = '';
-		$orderBy = '';
-		$limit = '';
-		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields,$table,$where,$groupBy,$orderBy,$limit);
+		$where .= t3lib_befunc::BEenableFields($table);
+		$where .= t3lib_befunc::deleteClause($table);
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields, $table, $where);
 		$resCount = $GLOBALS['TYPO3_DB']->sql_num_rows($res);
+
 		if ($resCount) {
-			while ( ($newsRecord = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) ) {
+			while (($newsRecord = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))) {
 
 				// if mode equals 'choose categories for indexing' (2). 1 = All
-				if($this->indexerConfig['index_news_category_mode'] == '2') {
+				if ($this->indexerConfig['index_news_category_mode'] == '2') {
 					$resCat = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query(
-						'tt_news_cat.uid',
-						'tt_news',
-						'tt_news_cat_mm',
-						'tt_news_cat',
-						' AND tt_news.uid = ' . $newsRecord['uid'] .
+						'tt_news_cat.uid', 'tt_news', 'tt_news_cat_mm', 'tt_news_cat', ' AND tt_news.uid = ' . $newsRecord['uid'] .
 						t3lib_befunc::BEenableFields('tt_news_cat') .
-						t3lib_befunc::deleteClause('tt_news_cat'),
-						'', '', ''
+						t3lib_befunc::deleteClause('tt_news_cat'), '', '', ''
 					);
-					if(is_resource($resCat)) {
+
+					if (is_resource($resCat)) {
 						$isInList = false;
-						while($newsCat = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($resCat)) {
+						while ($newsCat = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($resCat)) {
 							// if category was found in list, set isInList to true and break further processing.
-							if(t3lib_div::inList($this->indexerConfig['index_news_category_selection'], $newsCat['uid'])) {
+							if (t3lib_div::inList($this->indexerConfig['index_news_category_selection'], $newsCat['uid'])) {
 								$isInList = true;
 								break;
 							}
 						}
+
 						// if category was not fount stop further processing and loop with next news record
-						if(!$isInList) {
-							continue ;
+						if (!$isInList) {
+							continue;
 						}
 					}
 				}
@@ -134,9 +128,9 @@ class tx_kesearch_indexer_types_ttnews extends tx_kesearch_indexer_types {
 				$abstract = strip_tags($newsRecord['short']);
 				$content = strip_tags($newsRecord['bodytext']);
 
-					// add keywords to content if not empty
+				// add keywords to content if not empty
 				if (!empty($newsRecord['keywords'])) {
-					$content .= "\n".$newsRecord['keywords'];
+					$content .= "\n" . $newsRecord['keywords'];
 				}
 
 				// create content
@@ -149,61 +143,57 @@ class tx_kesearch_indexer_types_ttnews extends tx_kesearch_indexer_types {
 				$params = rawurldecode($params);
 
 				// create tags
-				if($this->indexerConfig['index_use_page_tags']) {
+				if ($this->indexerConfig['index_use_page_tags']) {
 					$tags = $this->pageRecords[intval($newsRecord['pid'])]['tags'];
-				} else $tags = '';
+				} else {
+					$tags = '';
+				}
 
 				$additionalFields = array();
 
-					// make it possible to modify the indexerConfig via hook
+				// make it possible to modify the indexerConfig via hook
 				$indexerConfig = $this->indexerConfig;
 
-					// hook for custom modifications of the indexed data, e. g. the tags
+				// hook for custom modifications of the indexed data, e. g. the tags
 				if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyNewsIndexEntry'])) {
-					foreach($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyNewsIndexEntry'] as $_classRef) {
+					foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyNewsIndexEntry'] as $_classRef) {
 						$_procObj = & t3lib_div::getUserObj($_classRef);
 						$_procObj->modifyNewsIndexEntry(
-							$title,
-							$abstract,
-							$fullContent,
-							$params,
-							$tags,
-							$newsRecord,
-							$additionalFields,
-							$indexerConfig
+							$title, $abstract, $fullContent, $params, $tags, $newsRecord, $additionalFields, $indexerConfig
 						);
 					}
 				}
 
 				// ... and store them
 				$this->pObj->storeInIndex(
-					$indexerConfig['storagepid'],    // storage PID
-					$title,                          // page title
-					'tt_news',                       // content type
-					$indexerConfig['targetpid'],     // target PID: where is the single view?
-					$fullContent,                    // indexed content, includes the title (linebreak after title)
-					$tags,                           // tags
-					$params,                         // typolink params for singleview
-					$abstract,                       // abstract
-					$newsRecord['sys_language_uid'], // language uid
-					$newsRecord['starttime'],        // starttime
-					$newsRecord['endtime'],          // endtime
-					$newsRecord['fe_group'],         // fe_group
-					false,                           // debug only?
-					$additionalFields                // additional fields added by hooks
+					$indexerConfig['storagepid'],     // storage PID
+					$title,                           // news title
+					'tt_news',                        // content type
+					$indexerConfig['targetpid'],      // target PID: where is the single view?
+					$fullContent,                     // indexed content, includes the title (linebreak after title)
+					$tags,                            // tags
+					$params,                          // typolink params for singleview
+					$abstract,                        // abstract
+					$newsRecord['sys_language_uid'],  // language uid
+					$newsRecord['starttime'],         // starttime
+					$newsRecord['endtime'],           // endtime
+					$newsRecord['fe_group'],          // fe_group
+					false,                            // debug only?
+					$additionalFields		  // additional fields added by hooks
 				);
 			}
 			$content = '<p><b>Indexer "' . $this->indexerConfig['title'] . '":</b><br />' . "\n"
-					. $resCount . ' News have been indexed.</p>' . "\n";
+				. $resCount . ' News have been indexed.</p>' . "\n";
 
 			$content .= $this->showErrors();
 			$content .= $this->showTime();
 		}
 		return $content;
 	}
+
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ke_search/indexer/types/class.tx_kesearch_indexer_types_ttnews.php'])	{
+if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ke_search/indexer/types/class.tx_kesearch_indexer_types_ttnews.php']) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ke_search/indexer/types/class.tx_kesearch_indexer_types_ttnews.php']);
 }
 ?>
