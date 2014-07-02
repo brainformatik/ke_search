b4c9e45823c475892925d481a9f9e643b67736f2
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-02 15:17:37 +0000

    task: add new hook for displaying additional filters in the backend (customFilterFlexformEntry).
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_search/trunk@85798 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Backend/class.user_filterlist.php
+++ Classes/Backend/class.user_filterlist.php
@@ -59,8 +59,14 @@ class user_filterlist {
 			$config['items'][] = array($row['title'], $row['uid']);
 		}
 
+		// hook for custom filter
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customFilterFlexformEntry'])) {
+			foreach($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customFilterFlexformEntry'] as $_classRef) {
+				$_procObj = & t3lib_div::getUserObj($_classRef);
+				$_procObj->customFilterFlexformEntry($config, $intString);
+			}
+		}
 	}
-
 
 
 	function getListOfAvailableFiltersForTCA(&$config) {
